2a942c2d2707d3ac8273f1c2ab2949236cb992c8
Hatem Hosny <hatemhosny@gmail.com>
2024-01-30 19:57:01 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 const args = process.argv.slice(2);
-
+const { encode } = require("js-base64");
 const core = require("@actions/core");
 const github = require("@actions/github");
+
+const toDataUrl = (content, type = "text/javascript") =>
+  `data:${type};charset=UTF-8;base64,` + encode(content, true);
 
 try {
   const fileMapInput = args[0].trim();
@@ -18,7 +21,7 @@ try {
 
     Object.keys(fileMap).forEach((key) => {
       const text = fs.readFileSync(fileMap[key], "utf8");
-      console.log(text);
+      console.log(toDataUrl(text));
     });
   }
 
